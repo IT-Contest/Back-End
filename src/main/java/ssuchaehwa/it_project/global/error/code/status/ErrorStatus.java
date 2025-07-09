99a62e433599d2d16cf45f85a17ce2aa69e7a667
seongjinYU <128691265+seongjinYU@users.noreply.github.com>
2025-07-09 16:12:41 +0900

    feat/auth : 토큰 재발급 로직 추가

--- src/main/java/ssuchaehwa/it_project/global/error/code/status/ErrorStatus.java
+++ src/main/java/ssuchaehwa/it_project/global/error/code/status/ErrorStatus.java
@@ -15,7 +15,12 @@ public enum ErrorStatus implements BaseErrorCode{
     _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
 
     // user
-    NO_SUCH_USER(HttpStatus.BAD_REQUEST, "USER_4001", "유저가 존재하지 않습니다.");
+    NO_SUCH_USER(HttpStatus.BAD_REQUEST, "USER_4001", "유저가 존재하지 않습니다."),
+
+    // token
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "TOKEN_401", "유효하지 않거나 만료된 refreshToken입니다."),
+    REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "TOKEN_404", "Redis에 해당 userId의 refreshToken이 존재하지 않습니다.");
+
 
     // quest
 
