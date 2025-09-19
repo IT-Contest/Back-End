8fe6f55aa7d0274505fc1fd585c3da7d158abe06
seongjinYU <128691265+seongjinYU@users.noreply.github.com>
2025-09-20 01:41:11 +0900

    뽀모도로 qa 반영

--- src/main/java/ssuchaehwa/it_project/domain/pomodoro/dto/PomodoroRequestDTO.java
+++ src/main/java/ssuchaehwa/it_project/domain/pomodoro/dto/PomodoroRequestDTO.java
@@ -1,10 +1,27 @@
 package ssuchaehwa.it_project.domain.pomodoro.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor
+import java.time.LocalDateTime;
+
 public class PomodoroRequestDTO {
-    private int durationMinutes; // 25분 고정도 가능
+    
+    @Getter
+    @NoArgsConstructor
+    public static class StartRequest {
+        private int durationMinutes; // 25분 고정도 가능
+    }
+    
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class PomodoroCompleteRequest {
+        private int sessionCount;
+        private int totalMinutes;
+        private LocalDateTime completedAt;
+    }
 }
